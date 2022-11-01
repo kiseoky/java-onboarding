c6cd4f1e5620b931d945df8ee96f07f88fe8dfb0
k-seok <rltjr1092@gmail.com>
2022-11-01 20:22:19 +0900

    문제 4 번역 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,31 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return translate(word);
+    }
+
+    private static String translate(String word) {
+        StringBuilder stringBuilder = new StringBuilder();
+        word.chars().forEach(chr->
+            stringBuilder.append(translate((char)chr))
+        );
+
+        return stringBuilder.toString();
+    }
+
+    public static char translate(char chr) {
+        if(!Character.isAlphabetic(chr))
+            return chr;
+
+        int zNum = (int)'Z';
+        int aNum = (int)'A';
+
+        if (Character.isLowerCase(chr)) {
+            zNum = (int)'z';
+            aNum = (int)'a';
+        }
+
+        int diff = zNum - chr;
+        return (char) (aNum+diff);
     }
 }
